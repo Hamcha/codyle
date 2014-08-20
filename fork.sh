4e8e98c9466c38dd27cda4c75aaa2fc405c2c57a
Hamcha <zikyky@gmail.com>
2014-08-21 00:25:03 +0200

    Search for commits

--- fork.sh
+++ fork.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
+
+# Get repo
+URL="https://api.github.com/search/repositories?q=language:c&sort=updated&order=desc&per_page=1"
+PAIR=`curl ${URL} | grep full_name | sed 's/.*"\([^"]*\)".*,/\1/ig'`
+IFS='/'
+read VICTIM REPO
+
+# Check if dir already exists (and quit if so)
+if [ -d $REPO ]; then
+	exit
+fi
+
+# Clone repo and be a bad guy
 hub clone ${VICTIM}/${REPO}
 cd ${REPO}
 hub fork
