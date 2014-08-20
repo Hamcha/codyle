0c2144af7b73093b304214c25849e96363c81a2a
Hamcha <zikyky@gmail.com>
2014-08-20 17:17:30 +0200

    Fixed astyle command

--- fork.sh
+++ fork.sh
@@ -2,10 +2,9 @@
 hub clone ${VICTIM}/${REPO}
 cd ${REPO}
 hub fork
-astyle --recursive \*.c \*.h
-astyle --recursive \*.cpp \*.h
+find . -regex ".*\.[c|cpp|h]" -exec astyle {} \;
 find . -name "*.orig" -delete
-git add \*
+git add *
 git commit -m "Improved coding style"
 git push ssh://git@github.com/${USER}/${REPO}.git
 hub pull-request -h master -f -F ~/prfile
